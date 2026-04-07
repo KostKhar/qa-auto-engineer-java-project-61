9cfe459b06f4e4a9dcb718ef54075f8674d37fe5
kostkhar <konstantin.kharik@onlinesup.com>
2026-04-07 19:05:08 +0300

    update main

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,25 +6,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("0 - Exit");
-        Scanner scanner = new Scanner(System.in);
-        if (scanner.hasNextInt()) {
-            int commandNumber = scanner.nextInt();
-            System.out.println("Your choice: " + commandNumber);
-            if (commandNumber == 0) {
-                return;
-            }
 
-            String gamerName = Cli.helloPlayerInGame();
-            if (commandNumber == 1) {
+        if (!scanner.hasNextInt()) {
+            System.out.println("Please choose a number between 0 and 1. Please retry the program.");
+            return;
+        }
+
+        int commandNumber = scanner.nextInt();
+        System.out.println("Your choice: " + commandNumber);
+
+        switch (commandNumber) {
+            case 0:
+                return;
+
+            case 1:
+                String gamerName = Cli.helloPlayerInGame();
                 EvenNumber evenNumber = new EvenNumber();
                 System.out.println(evenNumber.getAnswer(gamerName));
-            }
-        } else {
-            System.out.println("Please choose a number between 0 and 1."
-                    + " Retry the program please.");
+                break;
+
+            default:
+                System.out.println("Please choose a number between 0 and 1. Please retry the program.");
         }
     }
 }
