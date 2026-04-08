2140f1cf943fe8504beb44b59689fde2d545e0ba
kostkhar <konstantin.kharik@onlinesup.com>
2026-04-08 11:09:28 +0300

    update game.cast

--- app/src/main/java/hexlet/code/game/EvenNumber.java
+++ app/src/main/java/hexlet/code/game/EvenNumber.java
@@ -1,41 +1,44 @@
 package hexlet.code.game;
 
 import java.util.Random;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class EvenNumber extends Game {
     private final Scanner scanner = new Scanner(System.in);
     private final Random random = new Random();
+    private static final ResourceBundle MESSAGES =
+            ResourceBundle.getBundle("messages");
 
     public EvenNumber() {
         super();
     }
 
     public int getCode() {
-        int codeGame = 2;
+        codeGame = 2;
         return codeGame;
     }
 
     public String getAnswer(String gamerName) {
 
         for (int i = 0; i < 3; i++) {
-            int number = random.nextInt(100);
-            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-            System.out.println("Question: " + number);
-            System.out.print("Your answer: ");
+            int number = random.nextInt();
+            System.out.println(MESSAGES.getString("even.rule"));
+            System.out.println(MESSAGES.getString("even.question") + number);
+
             String answer = scanner.nextLine().trim().toLowerCase();
+            System.out.println(MESSAGES.getString("even.your_answer") + answer);
 
             boolean isEven = number % 2 == 0;
 
             if ((answer.equals("yes") && isEven) || (answer.equals("no") && !isEven)) {
-                System.out.println("Correct!");
+                System.out.println(MESSAGES.getString("even.correct"));
             } else {
                 String correct = isEven ? "yes" : "no";
-                return "'" + answer + "' is wrong answer ;(. Correct answer was '" + correct
-                        + "'.\nLet's try again, " + gamerName + "!";
+                return String.format(MESSAGES.getString("even.invalid"), answer, correct, gamerName);
             }
         }
-        return "Congratulations!";
+        return MESSAGES.getString("even.congratulations");
     }
 
 
