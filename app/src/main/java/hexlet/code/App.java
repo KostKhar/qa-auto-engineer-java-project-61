377dca3fa7b0f489d1a72b836142760fccf0bae7
kostkhar <konstantin.kharik@onlinesup.com>
2026-04-13 21:12:30 +0300

    add Engine class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,30 @@
 package hexlet.code;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class App {
+
+    static Logger logger = Logger.getLogger(App.class.getName());
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         String ganerName = Cli.helloPlayerInGame();
+
+        logger.info("Please enter the game number and press Enter.");
+        for (Game game : games) {
+            System.out.println(game.getCode() + " - " + game.getClass().getSimpleName());
+        }
+        logger.info(MESSAGES.getString("menu.exit"));
+
+        if (!scanner.hasNextInt()) {
+            logger.info(MESSAGES.getString("menu.invalid"));
+            return;
+        }
+
+        int commandNumber = scanner.nextInt();
+        logger.info("Your choice: " +commandNumber);
+
         Engine engine = new Engine();
 
         engine.run(ganerName);
