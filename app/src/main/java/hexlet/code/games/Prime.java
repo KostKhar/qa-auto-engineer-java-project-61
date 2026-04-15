f5ee3effbdad55634312ab42b62dfebb381c7d4c
kostkhar <konstantin.kharik@onlinesup.com>
2026-04-15 20:01:38 +0300

    update Engine, add  GCD, Prime, Progession

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -22,7 +22,9 @@ public final class Prime {
         }
 
         for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) return "no";
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return "no";
+            }
         }
         return "yes";
     }
